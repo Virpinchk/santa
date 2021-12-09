74529a09590fb93d716a700cbcff89956f6033d0
Virpinchk <91627169+Virpinchk@users.noreply.github.com>
2021-12-09 16:59:39 +0500

    Add files via upload

--- plaform.gd
+++ plaform.gd
@@ -5,12 +5,13 @@ var scam = false
 
 
 func _physics_process(delta):
-	if playernode.position.x > $endpoint.global_position.x and not scam:
+	if not scam and playernode.transform.origin.x > position.x - 200:
 		var nexplotform = load("res://platform.tscn").instance()
 		nexplotform.position = $endpoint.global_position 
+		nexplotform.rotation_degrees = clamp(rand_range(-30, 30)+global_rotation_degrees, -70, 70)
 		nexplotform.playernode = playernode
 		$"..".add_child(nexplotform) 
-		
+		scam = true		
 func _ready():
 	if not playernode:
 		playernode=get_node(plyer) # Replace with function body.
--- sani.gd
+++ sani.gd
@@ -4,6 +4,5 @@ var taga = 3000 #appel
 func _physics_process(delta):
 	applied_force=(transform.x*taga)if Input.is_action_pressed("GO") else Vector2()#appel
 
-
 func _ready():
 	pass 
